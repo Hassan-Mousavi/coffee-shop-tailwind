a3d1a4a23362837579452fcaa65c07b4ed4069a8
Hassan-Mousavi <hassanmousavi400@gmail.com>
2025-12-02 16:29:10 +0330

    finish header responsive

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,9 @@ const subMenu = document.querySelector(".sub-menu");
 const barsOpenIcon = document.querySelector(".bars-icon");
 const barsCloseIcon = document.querySelector(".x-mark");
 const nav = document.querySelector(".nav");
+const cardOpenBtn = document.querySelector(".card-icon");
+const cardClosebtn = document.querySelector(".card-close-btn");
+const card = document.querySelector(".card");
 const overlay = document.querySelector(".overlay");
 
 // change dark mode to light mode
@@ -23,19 +26,35 @@ submenuBtn.addEventListener("click", function (e) {
   e.currentTarget.parentElement.classList.toggle("text-orange-300");
   subMenu.classList.toggle("submenu--open");
 });
-// bars icon
-barsOpenIcon.addEventListener("click", function () {
-  nav.classList.remove("-right-64");
-  nav.classList.add("right-0");
-  overlay.classList.add("overlay--visible");
-});
-
 function closeNav() {
   nav.classList.remove("right-0");
   nav.classList.add("-right-64");
   overlay.classList.remove("overlay--visible");
 }
+//
+function closeCard() {
+  card.classList.remove("left-0");
+  card.classList.add("-left-64");
+  overlay.classList.remove("overlay--visible");
+}
+// bars icon
+barsOpenIcon.addEventListener("click", function () {
+  nav.classList.remove("-right-64");
+  nav.classList.add("right-0");
+  overlay.classList.add("overlay--visible");
+  overlay.addEventListener("click", closeNav);
+});
+// card open btn
+cardOpenBtn.addEventListener("click", function () {
+  card.classList.remove("-left-64");
+  card.classList.add("left-0");
+  overlay.classList.add("overlay--visible");
+  overlay.addEventListener("click", closeCard);
+});
+
+
 // bars close btn
 barsCloseIcon.addEventListener("click", closeNav);
+cardClosebtn.addEventListener("click", closeCard);
 // overlay close menu bars mobile
-overlay.addEventListener("click", closeNav);
+
